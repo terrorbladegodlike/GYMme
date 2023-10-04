ea513c09e9580ade057b4999505459838f874181
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-10-04 21:25:28 +0300

    Adding a "About" component.
    Added "About" component. Animation using "data-aos".

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
 
+// Import Data
+import { about } from '../data'
+
+// Import Icons
+import { IoIosArrowDroprightCircle } from 'react-icons/io'
+
 const About = () => {
+
+  // Destructure About Data
+  const { icon, title, subtitle1, subtitle2, link } = about
+
   return (
-    <div>About</div>
+    <section className='py-[80px] md:py-[110px] lg:pt-[140px] lg:pb-[180px]'>
+      <div className="container mx-auto px-[20px] lg:px-[135px]">
+        {/* Section Title */}
+        <div
+          data-aos='fade-up'
+          data-aos-delay='100'
+          className='section-title-group justify-start'
+        >
+          <img src={icon} alt="icon" />
+          <h2 className='h2 section-title'>
+            {title}
+            <span className='text-primary-200'>.</span>
+          </h2>
+        </div>
+        {/* Subtitle 1 */}
+        <p
+          data-aos='fade-up'
+          data-aos-delay='200'
+          className='md:text-body-md mb-12'
+        >
+          {subtitle1}
+        </p>
+        {/* Subtitle 2 */}
+        <p
+          data-aos='fade-up'
+          data-aos-delay='300'
+          className='md:text-body-md mb-8'
+        >
+          {subtitle2}
+        </p>
+        {/* Link */}
+        <div
+          data-aos='fade-up'
+          data-aos-delay='400'
+        >
+          <a
+            className='link flex items-center gap-x-4 hover:gap-x-6 transition-all'
+            href="#!"
+          >
+            {link}
+            <IoIosArrowDroprightCircle className='text-2xl' />
+          </a>
+        </div>
+      </div>
+    </section>
   )
 }
 
