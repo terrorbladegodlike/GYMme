a4af5cb832297e2914cafc812611b0f8e74bc406
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-10-04 21:48:48 +0300

    Adding a "Workuots" and "WorkoutSlider" components.
    Added "Workouts" and "WorkuotSlider" components. Animation using "data-aos". Slider made with "swiper".

--- src/components/WorkoutSlider.jsx
+++ src/components/WorkoutSlider.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
 
+// Import Data
+import { workouts } from '../data'
+
+// Import Swiper React Components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper Styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import '../workoutSlider.css'
+
+// Import Required Modules
+import { Navigation } from 'swiper'
+
 const WorkoutSlider = () => {
+
+  // Destructure Workout Data
+  const { programs } = workouts
+
   return (
-    <div>WorkoutSlider</div>
+    <Swiper
+      slidesPerView={2}
+      spaceBetween={32}
+      navigation={true}
+      breakpoints={{
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      }}
+      modules={[Navigation]}
+      className='workoutSlider'
+    >
+      {
+        programs.map((program, index) => {
+
+          // Destructure Programs Data
+          const { image, name } = program
+
+          return (
+            <SwiperSlide
+              className='max-w-[320px] max-h-[320px] relative'
+              key={index}
+            >
+              <img
+                className='w-full h-full object-cover'
+                src={image}
+                alt="img"
+              />
+              <div className='absolute left-[20px] bottom-[20px] bg-white h-[26px] px-[14px] flex items-center rounded-[1px]'>
+                <div className='font-primary font-semibold text-sm text-neutral-500'>
+                  {name}
+                </div>
+              </div>
+            </SwiperSlide>
+          )
+        })
+      }
+    </Swiper >
   )
 }
 
--- src/components/Workouts.jsx
+++ src/components/Workouts.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
+// Import Data
+import { workouts } from '../data'
+
+// Import Components
+import WorkoutSlider from './WorkoutSlider';
+
 const Workouts = () => {
+
+  // Destructure Workout Data
+  const { title, icon } = workouts
+
   return (
-    <div>Workouts</div>
+    <section className='section'>
+      {/* Section Title */}
+      <div
+        data-aos='fade-up'
+        data-aos-delay='200'
+        className='section-title-group max-w-[540px] mx-auto px-4 lg:px-0'
+      >
+        <img src={icon} alt="icon" />
+        <h2 className='h2 section-title'>
+          {title}
+          <span className='text-primary-200'>.</span>
+        </h2>
+      </div>
+      {/* Slider */}
+      <div
+        data-aos='fade-up'
+        data-aos-delay='300'
+      >
+        <WorkoutSlider />
+      </div>
+    </section>
   )
 }
 
