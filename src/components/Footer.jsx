c38a1cc2b44a76ac48f6b2060cec8438822c0282
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-10-07 12:27:35 +0300

    Adding a "Footer" component.
    Addition of "Footer" component. Added animation using "data-aos".

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+// Import Data 
+import { footer } from '../data'
+
 const Footer = () => {
+
+  // Destructure Footer Data
+  const { logo, copyrightText } = footer
+
   return (
-    <div>Footer</div>
+    <footer className='bg-neutral-500 h-[125px] md:h-[195px] px-[20px]'>
+      <div
+        data-aos='fade-up'
+        data-aos-delay='200'
+        className="container mx-auto h-full flex justify-between items-center md:items-end mb:pb-[50px]"
+      >
+        {/* Logo */}
+        <a href="#!">
+          <img src={logo} alt="logo" />
+        </a>
+        {/* Copyright Text */}
+        <p className='text-neutral-300 text-sm'>
+          {copyrightText}
+        </p>
+      </div>
+    </footer>
   )
 }
 
