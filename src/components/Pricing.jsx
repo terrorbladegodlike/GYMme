c3767769821fc9007a1d87a7a41760bec1edccc1
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-10-07 10:54:37 +0300

    Adding a "Pricing" and "PlanList" components.
    Addition of "Pricing" and "PlanList" components. Added workout plan selection with "useState" and added animation with "data-aos".

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
+// Import Data
+import { pricing } from '../data'
+
+// Import Components
+import PlanList from './PlanList';
+
 const Pricing = () => {
+
+  // Destructure Pricing Data
+  const { icon, title, plans } = pricing
+
   return (
-    <div>Pricing</div>
+    <section className='section'>
+      {/* Section Title */}
+      <div
+        data-aos='fade-up'
+        data-aos-offset='200'
+        data-aos-delay='200'
+        className='section-title-group max-w-[540px] mx-auto px-4 lg:px-0'
+      >
+        <img src={icon} alt="icon" />
+        <h2 className='h2 section-title'>
+          {title}
+          <span className='text-primary-200'>.</span>
+        </h2>
+      </div>
+      {/* Plan List */}
+      <PlanList plans={plans} />
+    </section>
   )
 }
 
